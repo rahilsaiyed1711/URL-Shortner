94af1332f664a248050defe9294f89e3a9399f20
Rahil RiyazAhmed Saiyed <102587430+rahilsaiyed1711@users.noreply.github.com>
2024-10-29 16:41:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8001;
+const PORT = process.env.PORT || 8001;
 const URL = require('./models/url');
 const { connectToMongo } = require('./connect');
 const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth')
